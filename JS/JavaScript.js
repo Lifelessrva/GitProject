9a830e14b02a9e0d89381d9fcb5aeeafbd0c7572
Bob Adams <bob@sideviewrva.com>
2017-11-13 14:40:49 -0500

    Added form element to html, corrected indentations.

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -5,12 +5,12 @@ var totalElem = document.getElementById("total");
 myBtn.addEventListener('click', calc);
 
 function calc(e) {
-  e.preventDefault();
-  var input = document.getElementById('bill');
-  var bill = parseFloat(input.value);
-  var tip = bill * 0.15;
-  var total = bill + tip;
-  
-  tipElem.textContent = "$"+(tip).toFixed(2);
-  totalElem.textContent = "$"+(total).toFixed(2);  
+    e.preventDefault();
+    var input = document.getElementById('bill');
+    var bill = parseFloat(input.value);
+    var tip = bill * 0.15;
+    var total = bill + tip;
+
+    tipElem.textContent = "$" + (tip).toFixed(2);
+    totalElem.textContent = "$" + (total).toFixed(2);
 }
