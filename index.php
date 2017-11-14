bf1e3ca829645115f07550a382f805181007ed47
Bob Adams <bob@sideviewrva.com>
2017-11-14 12:14:37 -0500

    Added PHPMailer files, installing PHP 11/14-12:15p.m.

--- index.php
+++ index.php
@@ -27,11 +27,7 @@ try {
     $mail->addReplyTo('info@example.com', 'Information');
     $mail->addCC('cc@example.com');
     $mail->addBCC('bcc@example.com');
-
-    //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
+    
     //Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Here is the subject';
